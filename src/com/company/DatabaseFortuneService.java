0e3c282e6c4ccc730d6f265f7e661738f21639f4
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-16 11:38:02 +0300

    Database service implemented

---
+++ src/com/company/DatabaseFortuneService.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements IFortuneService{
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
